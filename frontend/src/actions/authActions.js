8f96722e93696de15761d82af0c9e178246cd541
imane <iaitlmaalem@gmail.com>
2020-05-07 12:06:33 +0000

    commit

--- frontend/src/actions/authActions.js
+++ frontend/src/actions/authActions.js
@@ -9,7 +9,7 @@ import {
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post("/api/users/register", userData)
+    .post("/api/utilisateur/register", userData)
     .then(res => history.push("/login")) // re-direct to login on successful register
     .catch(err =>
       dispatch({ //dispatch to send actions to our reducers
@@ -21,7 +21,7 @@ export const registerUser = (userData, history) => dispatch => {
 // Login - get user token
 export const loginUser = userData => dispatch => {
   axios
-    .post("/api/users/login", userData)
+    .post("/api/utilisateur/login", userData)
     .then(res => {
       // Save to localStorage
 // Set token to localStorage
