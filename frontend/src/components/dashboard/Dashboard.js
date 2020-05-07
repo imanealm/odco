4413ac8f6739b5cf1c4b30c260f400c8a4933e8c
imane <iaitlmaalem@gmail.com>
2020-05-07 14:58:11 +0000

    commit

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -15,10 +15,6 @@ return (
           <div className="col s12 center-align">
             <h4>
               <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app
-              </p>
             </h4>
             <button
               style={{
